69fbcd387daf669bdb3af7c0d2050c25d8029392
Pawel Piecuch <piecuch.pawel@gmail.com>
2025-06-02 21:05:27 +0200

    frmaebuffer rotation

--- lvgl-7.11.0/lv_drivers/display/fbdev.c
+++ lvgl-7.11.0/lv_drivers/display/fbdev.c
@@ -129,7 +129,7 @@ void fbdev_init(void) {
 	}
 #endif /* USE_BSD_FBDEV */
 
-	printf("%dx%d, %dbpp, rotate:%d\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel, vinfo.rotate);
+	printf("%dx%d, line length:%d, %dbpp, rotate:%d\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel, finfo.line_length, vinfo.rotate);
 
 	// Figure out the size of the screen in bytes
 	screensize = finfo.smem_len; //finfo.line_length * vinfo.yres;
@@ -190,7 +190,7 @@ void fbdev_flush(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_p)
 	else if (vinfo.bits_per_pixel == 16) {
 		uint16_t *fbp16 = (uint16_t *)fbp;
 		if (1) {
-			rotate_area_cw((uint16_t *)color_p, fbp, vinfo.xres, act_x1, act_y1, act_x2, act_y2);
+			rotate_area_cw((uint16_t *)color_p, fbp16, vinfo.xres, act_x1, act_y1, act_x2, act_y2);
 		} else {
 			int32_t y;
 			for (y = act_y1; y <= act_y2; y++) {
@@ -248,12 +248,12 @@ void fbdev_get_sizes(uint32_t *width, uint32_t *height) {
  *   STATIC FUNCTIONS
  **********************/
 
-static inline void rotate_area_cw(const int16_t *in, int16_t *out, int32_t dim, int32_t area_x1, int32_t area_y1, int32_t area_x2, int32_t area_y2) {
+static inline void rotate_area_cw(const int16_t *in, int16_t *out, int32_t out_height, int32_t line_length, int32_t area_x1, int32_t area_y1, int32_t area_x2, int32_t area_y2) {
 	int i, j;
 
 	for (i = area_y1; i < area_y1; i++) {
 		for (j = area_x1; j < area_x2; j++) {
-            out[i,j] = in[j,dim-i];
+            out[i+j*line_length] = in[j+(out_height-i)*line_length];
 		}
 	}
 }
