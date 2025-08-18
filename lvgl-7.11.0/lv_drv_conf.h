297c5a53532948ecc0ecc6d75efa2f0ccb45af20
Pawel Piecuch <piecuch.pawel@gmail.com>
2025-08-19 00:42:59 +0200

    update kb driver

--- lvgl-7.11.0/lv_drv_conf.h
+++ lvgl-7.11.0/lv_drv_conf.h
@@ -374,7 +374,7 @@
 #endif
 
 #if USE_LIBINPUT
-#  define LIBINPUT_NAME   "/dev/input/event0"        /*You can use the "evtest" Linux tool to get the list of devices and test them*/
+#  define LIBINPUT_NAME   "/dev/input/event6"        /*You can use the "evtest" Linux tool to get the list of devices and test them*/
 #endif  /*USE_LIBINPUT*/
 
 /*-------------------------------------------------
@@ -394,7 +394,7 @@
 #endif // __linux__
 
 #if USE_EVDEV
-#  define EVDEV_NAME   "/dev/input/event0"        /*You can use the "evtest" Linux tool to get the list of devices and test them*/
+#  define EVDEV_NAME   "/dev/input/event6"        /*You can use the "evtest" Linux tool to get the list of devices and test them*/
 #  define EVDEV_SWAP_AXES         0               /*Swap the x and y axes of the touchscreen*/
 
 #  define EVDEV_CALIBRATE         0               /*Scale and offset the touchscreen coordinates by using maximum and minimum values for each axis*/
